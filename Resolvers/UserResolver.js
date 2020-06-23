b87a29dbf5917d332a73c023bbe1aacb5c6c2c6d
enigmaOsiris <enigma.osiris@gmail.com>
2020-06-23 18:44:15 -0500

    auth users

--- Resolvers/UserResolver.js
+++ Resolvers/UserResolver.js
@@ -1,11 +1,11 @@
 const { neo4jgraphql } = require("neo4j-graphql-js");
-const jwt = require('jsonwebtoken');
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const crearToken=(usuario, secreta, expiresIn)=>{
-  const {id, email, password} = usuario;
-  return jwt.sign( { id } ,secreta, {expiresIn})
-}
-
+const crearToken = (usuario, secreta, expiresIn) => {
+  const { id, email, password } = usuario;
+  return jwt.sign({ id }, secreta, { expiresIn });
+};
 
 export default {
   Query: {
@@ -17,25 +17,75 @@ export default {
         console.log("Error");
       }
     },
-    obtenerUsuario:async (_,{token})=>{
-      const usuarioId = jwt.verify(token,"secretWord")
-      return usuarioId
-    }
+    obtenerUsuario: async (_, { token }) => {
+      const usuarioId = jwt.verify(token, "secretWord");
+      return usuarioId;
+    },
   },
 
   Mutation: {
-    autenticarUsuario: async (_,{input})=>{
-      const {email, password}=input;
-      const existeUsuario=input;
-      //si el usuario existe
-      
-      //revisar password
+    NuevoUsuario: async (obj, { input }, context) => {
+      try {
+        console.log("Aca");
 
-      //crear token
-      return {
-        token: crearToken(existeUsuario,"secretWord",'24h')
+        const session = context.driver.session();
+        //console.log(session)
+        console.log(input);
+        const salt = await bcryptjs.getSalt(10);
+        input.password = await bcryptjs.hash(input.password, salt);
+        console.log(input.password);
+        return session
+          .run(
+            "CREATE (n:User {name:$name, lastName:$lastName,email:$email,password:$password,active:$active,exists:$exists,employeeNumber:$employeeNumber}) RETURN n",
+            {
+              name: input.name,
+              lastName: input.lastName,
+              email: input.email,
+              password: input.password,
+              active: input.active,
+              exists: input.exists,
+              employeeNumber: input.employeeNumber,
+            }
+          )
+          .then((re) => {
+            session.close();
+            return input;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } catch (e) {
+        console.log(e);
       }
-
+    },
+    autenticarUsuario: async (_, { input }, context) => {
+      console.log("aca");
+      try {
+        console.log("aca");
+        console.log(input);
+        const { email, password } = input;
+        const session = context.driver.session();
+        const r=session
+          .run(
+            "MATCH(n:User {email:$email}) RETURN n",
+            {
+              email: input.email,
+            }
+          )
+          .then((re) => {
+            console.log("re"+JSON.stringify(re.records));
+            if (re.records.length) {
+              console.log("existe un usuario");
+              
+            } else {
+              console.log("no existe");
+            }
+          }).catch((err)=>{
+            console.log("catch error: "+err);
+          })
+      } catch (error) {
+        console.log(error);
+      }
     },
     mergeUserToDepartmen: async (obj, args, context, info) => {
       try {
@@ -50,7 +100,6 @@ export default {
       try {
         console.log(info);
         const resp = await neo4jgraphql(obj, args, context, info);
-
         return resp;
       } catch (error) {
         console.log("Error");
